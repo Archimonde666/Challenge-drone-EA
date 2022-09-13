533e1d8e81b15c35744c83764582158e5ad717f3
pbag47 <pierre.bagnara@ecole-air.fr>
2022-09-13 17:33:17 +0200

    Removed redundant class

--- subsys_visual_control.py
+++ subsys_visual_control.py
@@ -1,12 +1,6 @@
 from parameters import DEG2RAD, RAD2DEG
 import numpy as np
-
-
-class rc_status:
-    a = 0
-    b = 0
-    c = 0
-    d = 0
+from subsys_read_user_input import RCStatus
 
 
 class VisualControl:
@@ -21,35 +15,35 @@ class VisualControl:
     @classmethod
     def run(cls, target_marker, drone_status):
         if target_marker.id == -1:
-            rc_status.c = 0
-            rc_status.d = int(0.99*rc_status.d)    # yaw_velocity
-            rc_status.a = int(0.99*rc_status.a)    # left_right_velocity
+            RCStatus.c = 0
+            RCStatus.d = int(0.99*RCStatus.d)    # yaw_velocity
+            RCStatus.a = int(0.99*RCStatus.a)    # left_right_velocity
             # wait for the drone to pass the last Gate
             if cls.cmp > 10:
-                rc_status.b = int(0.99*rc_status.b)  # for_back_velocity
+                RCStatus.b = int(0.99*RCStatus.b)  # for_back_velocity
 
             cls.cmp = cls.cmp + 1
-            return rc_status
+            return RCStatus
         cls.cmp = 0
         # Get the angle and the distance between the marker and the drone
         phi = int(target_marker.m_angle * RAD2DEG)
         distance = target_marker.m_distance
 
         # Yaw velocity control
-        rc_status.d = int(cls.KP_YAW_CTRL * phi)
+        RCStatus.d = int(cls.KP_YAW_CTRL * phi)
 
         # Left/Right velocity control
-        DX = distance * np.sin(phi*DEG2RAD)
-        rc_status.a = int(cls.KP_LR_CTRL * DX)
+        dx = distance * np.sin(phi*DEG2RAD)
+        RCStatus.a = int(cls.KP_LR_CTRL * dx)
 
         # Forward/Backward velocity control
         rb_threshold = 40
-        rc_status.b = rb_threshold - int(rb_threshold * abs(phi)/70)
+        RCStatus.b = rb_threshold - int(rb_threshold * abs(phi)/70)
 
         # Up/Down velocity control
-        rc_status.c = 0
+        RCStatus.c = 0
 
-        return rc_status
+        return RCStatus
 
     @classmethod
     def stop(cls):
