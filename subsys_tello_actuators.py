98df266b67e757bc1de0d857b8c8205eba21a6ff
pbag47 <pierre.bagnara@ecole-air.fr>
2022-09-14 13:14:14 +0200

    subsys_tello_actuators.py : added type-hints and docstring

--- subsys_tello_actuators.py
+++ subsys_tello_actuators.py
@@ -1,18 +1,23 @@
+from DJITelloPy.djitellopy.tello import Tello
+from subsys_read_user_input import RCStatus
 
 
 class TelloActuators:
-    TELLO = None
+    """
+    Sends the velocity commands to the Tello
+    """
+    TELLO: Tello = None
 
     @classmethod
-    def setup(cls, tello):
+    def setup(cls, tello: Tello):
         cls.TELLO = tello
 
     @classmethod
-    def run(cls, rc_status):
+    def run(cls, rc_status: RCStatus):
         cls.update_rc_command(rc_status)
 
     @classmethod
-    def update_rc_command(cls, rc_status):
+    def update_rc_command(cls, rc_status: RCStatus):
         """Update routine. Send velocities to Tello.
         """
         cls.TELLO.send_rc_control(
