6a2601f13619ea68248cb2c575b134b26c0d25f8
pbag47 <pierre.bagnara@ecole-air.fr>
2022-09-15 13:11:43 +0200

    parameters.py: added type-hints

--- parameters.py
+++ parameters.py
@@ -2,36 +2,36 @@ import math
 
 
 # color parameters
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
+RED: tuple = (255, 0, 0)
+BLUE: tuple = (0, 0, 255)
 
-DEG2RAD = math.pi/180
-RAD2DEG = 180/math.pi
+DEG2RAD: float = math.pi/180
+RAD2DEG: float = 180/math.pi
 
-FPS = 120
-IMG_SIZE = (640, 480)
-DRONE_POS = (IMG_SIZE[0]//2, 480)
-SCREEN_SIZE = (800, 480)
+FPS: int = 120
+IMG_SIZE: tuple = (640, 480)
+DRONE_POS: tuple = (IMG_SIZE[0]//2, 480)
+SCREEN_SIZE: tuple = (800, 480)
 
 
 class ENV:
-    REAL = 0
-    SIMULATION = 1
-    DEBUG = 2
-    status = SIMULATION
+    REAL: int = 0
+    SIMULATION: int = 1
+    DEBUG: int = 2
+    status: int = SIMULATION
 
 
 class RUN:
-    STOP = False
-    START = True
+    STOP: bool = False
+    START: bool = True
 
 
 class MODE:
-    EMERGENCY = 0
-    TAKEOFF = 1
-    LAND = 2
-    FLIGHT = 3
+    EMERGENCY: int = 0
+    TAKEOFF: int = 1
+    LAND: int = 2
+    FLIGHT: int = 3
 
 
 class RunStatus:
-    value = RUN.STOP
+    value: bool = RUN.STOP
